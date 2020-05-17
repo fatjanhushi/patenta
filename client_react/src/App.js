804dc366246a4a7511d487e8cd2b69eb72ad4767
fatjanhushi <me@fatjoni.com>
2020-05-18 00:04:19 +0200

    test auto deploy3

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React test3
+          Learn React test4
         </a>
       </header>
     </div>
