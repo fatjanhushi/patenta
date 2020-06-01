2163ccb37b07957a9f8f8f86b6bd3e7fcf7f6d1d
Fatjon Hushi <me@fatjoni.com>
2020-06-01 14:10:37 +0200

    test678

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Fatjoni gezuar ditelindjen 2020!!!!!!');
+  res.send('Fatjoni gezuar ditelindjen 2020!!!!!!!!');
 });
 
 var server = app.listen(process.env.PORT || 5000, () =>
